b09a37d179257200cf731794f5df3a361331f79e
Takahito Yokouchi <taka@Takahitos-MacBook-Air.local>
2025-01-02 22:16:21 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const colorButton = document.getElementById("colorButton");
+const colors = ["#fef3c7", "#d1fae5", "#ede9fe", "#fee2e2"];
+colorButton.addEventListener("click", () => {
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+});
+
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach(link => {
+    link.addEventListener("click", event => {
+        event.preventDefault();
+        const targetId = link.getAttribute("href").substring(1);
+        const targetSection = document.getElementById(targetId);
+        targetSection.scrollIntoView({ behavior: "smooth"});
+    });
+});
