b593811166e852477a07393a25f54a1d024ca937
Takahito Yokouchi <taka@Takahitos-MacBook-Air.local>
2025-01-02 23:24:19 +0900

    Update all files withlatest changes

--- script.js
+++ script.js
@@ -14,3 +14,13 @@ navLinks.forEach(link => {
         targetSection.scrollIntoView({ behavior: "smooth"});
     });
 });
+
+document.addEventListener("scroll", ()=>  {
+    const projects = document.querySelectorAll(".project");
+    projects.forEach((project) => {
+        const rect = project.getBoundingClientRect();
+        if (rect.top < window.innerHeight) {
+            project.classList.add("visible");
+        }
+    });
+});
