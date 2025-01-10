15ae267e2fc09f7641a0706d022897265a463b04
Takahito Yokouchi <taka@Takahitos-MacBook-Air.local>
2025-01-10 22:21:43 +0900

    Update

--- script.js
+++ script.js
@@ -24,3 +24,17 @@ document.addEventListener("scroll", ()=>  {
         }
     });
 });
+
+const scrollToTopButton = document.getElementById("scrollToTop");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 200) { // 200px以上スクロールしたら表示
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+});
+
+scrollToTopButton.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" }); // スムーズにスクロール
+});
